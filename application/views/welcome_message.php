b442afcc9428a828da6be05b277745ce5cf88d7f
unknown <rksaudwls123@naver.com>
2024-06-19 17:58:21 +0900

    코드나이터 설치 및 기초 설정

---
+++ application/views/welcome_message.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+</head>
+<body>
+	Welcome CodeIgniter!! 코드이그나이터에 오신걸 환영합니다.
+</body>
+</html>
